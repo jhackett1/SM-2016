fb50a75b249a3c31c23ad245a5be7f5ba3955761
jhackett1 <hello@joshuahackett.com>
2016-06-22 21:21:16 +0100

    eh

--- footer.php
+++ footer.php
@@ -4,7 +4,10 @@
     <nav>
       <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
     </nav>
+
     <p>© University of Westminster Students' Union <?php echo date("Y"); ?></p>
+
+    <img id="union-logo" src="wp-content/themes/SM-2016/img/uwsu.png"/>
   </div>
 </footer>
 
